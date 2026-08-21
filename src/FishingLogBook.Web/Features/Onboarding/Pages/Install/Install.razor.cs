e4880fc764c58dbb006edded8aba774d9369fe7e
Eamonn Connolly <eamonn@evolving.technology>
2026-08-21 19:43:56 +0400

    FLB#111 | Onboardingn tidy up

--- src/FishingLogBook.Web/Features/Onboarding/Pages/Install/Install.razor.cs
+++ src/FishingLogBook.Web/Features/Onboarding/Pages/Install/Install.razor.cs
@@ -1,4 +1,3 @@
-using FishingLogBook.Web.Browser.Install;
 using FishingLogBook.Web.Localization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
@@ -7,32 +6,5 @@ namespace FishingLogBook.Web.Features.Onboarding.Pages.Install;
 
 public partial class Install : ComponentBase
 {
-    private bool _isLoading = true;
-    private InstallState _state = new(false, false, false, false);
-
-    [Inject] private IInstallService InstallService { get; set; } = default!;
     [Inject] private IStringLocalizer<UiStrings> Loc { get; set; } = default!;
-
-    protected override async Task OnInitializedAsync()
-    {
-        await RefreshAsync();
-    }
-
-    private async Task InstallAsync()
-    {
-        await InstallService.PromptAsync(CancellationToken.None);
-        await RefreshAsync();
-    }
-
-    private async Task RefreshAsync()
-    {
-        try
-        {
-            _state = await InstallService.GetStateAsync(CancellationToken.None);
-        }
-        finally
-        {
-            _isLoading = false;
-        }
-    }
 }
